0c71fb86da580f6573113837b8ca4417381cb433
363434879 <363434879@gotvdsb.ca>
2026-02-05 13:19:10 -0500

    Halfway!!

--- Topic 2-Numbers And Math/Program.cs
+++ Topic 2-Numbers And Math/Program.cs
@@ -5,7 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-
+            Console.WriteLine("I will now count my chickens:");
+            // Asterix (*) is for multiplication
+            Console.WriteLine("Hens " + (10 + 5 * 2));
+            // Slash (/) is for division
+            Console.WriteLine("Roosters " + (25 + 30 / 6));
+            // shows (six6)
+            Console.WriteLine("six" + 6);
+            //Shows (6 + 6)
+            Console.WriteLine("6 + 6");
+            //Shows (66)
+            Console.WriteLine("6" + 6);
+            //Shows (12)
+            Console.WriteLine(6 + 6);
+            Console.WriteLine("Now I will count the eggs:");
+            Console.WriteLine(11 / 2);
         }
     }
 }
