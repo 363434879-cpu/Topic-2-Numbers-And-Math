6dd37b8c15acfbb5370265d4f3b93886e03c0639
363434879 <363434879@gotvdsb.ca>
2026-02-05 13:04:22 -0500

    Add project files.

---
+++ Topic 2-Numbers And Math/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Topic_2_Numbers_And_Math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
