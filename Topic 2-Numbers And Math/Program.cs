634181332328532b638910efcdb2bcfbcd589e3b
363434879 <363434879@gotvdsb.ca>
2026-02-09 12:29:51 -0500

    Doing Task!

--- Topic 2-Numbers And Math/Program.cs
+++ Topic 2-Numbers And Math/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            //Naika
+            // % = modulus operator gives the remainder of a division
             Console.WriteLine("Hello, World!");
             Console.WriteLine("I will now count my chickens:");
             // Asterix (*) is for multiplication
@@ -19,7 +21,18 @@
             //Shows (12)
             Console.WriteLine(6 + 6);
             Console.WriteLine("Now I will count the eggs:");
+            //Division shows (5) just rounds to the nearest whole number
             Console.WriteLine(11 / 2);
-        }
+            //Shows (5.5)
+            Console.WriteLine(11.0 / 2);
+            Console.WriteLine("Is it true that 3 + 2 < 5 - 7?");
+            Console.WriteLine(3 + 2 < 5 - 7);
+            Console.WriteLine("What is 3 + 2? " + (3 + 2));
+            Console.WriteLine("What is 5 - 7? " + (5 - 7));
+            Console.WriteLine("Oh, that's why it's " + (3 + 2 < 5 - 7));
+
+            //Programming Task:
+
+        } 
     }
 }
